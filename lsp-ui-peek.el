db896771b93db9c7660ce5bb6c0f9dd4fe8b3550
Sebastien Chapuis <sebastien@chapu.is>
2017-12-31 02:56:23 +0100

    [lsp-ui-peek] Exit the peek on undefined key

--- lsp-ui-peek.el
+++ lsp-ui-peek.el
@@ -465,6 +465,7 @@ X."
     (define-key map (kbd "<tab>") 'lsp-ui-peek--toggle-file)
     (define-key map (kbd "q") 'lsp-ui-peek--abort)
     (define-key map (kbd "RET") 'lsp-ui-peek--goto-xref)
+    (define-key map [t]'lsp-ui-peek--abort)
     (setq lsp-ui-peek-mode-map map)))
 
 (defun lsp-ui-peek--abort ()
