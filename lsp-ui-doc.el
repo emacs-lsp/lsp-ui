374a6d22791d370d93b284b6c98240c3156d3d86
Sebastien Chapuis <sebastien@chapu.is>
2018-01-20 05:18:18 +0100

    [lsp-ui-doc] Set max height/width of the frame #58

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -76,6 +76,16 @@ ble `lsp-ui-doc-frame-parameters'"
   :type 'color
   :group 'lsp-ui-doc)
 
+(defcustom lsp-ui-doc-max-width 150
+  "Maximum number of columns of the frame."
+  :type 'integer
+  :group 'lsp-ui-doc)
+
+(defcustom lsp-ui-doc-max-height 30
+  "Maximum number of lines in the frame."
+  :type 'integer
+  :group 'lsp-ui-doc)
+
 (defface lsp-ui-doc-header
   '((t :foreground "black"
        :background "deep sky blue"))
@@ -313,8 +323,8 @@ BUFFER is the buffer where the request has been made."
 (defun lsp-ui-doc--resize-buffer ()
   "If the buffer's width is larger than the current window, resize it."
   (let* ((window-width (window-width))
-         (fill-column (- window-width 5)))
-    (when (> (lsp-ui-doc--buffer-width) window-width)
+         (fill-column (min lsp-ui-doc-max-width (- window-width 5))))
+    (when (> (lsp-ui-doc--buffer-width) (min lsp-ui-doc-max-width window-width))
       (lsp-ui-doc--with-buffer
        (fill-region (point-min) (point-max))))))
 
@@ -341,6 +351,8 @@ START-Y is the position y of the current window."
           (window (frame-root-window frame))
           ((width . height) (window-text-pixel-size window nil nil 10000 10000))
           (width (+ width (* (frame-char-width frame) 2))) ;; margins
+          (char-h (frame-char-height))
+          (height (min (- (* lsp-ui-doc-max-height char-h) (/ char-h 2)) height))
           (frame-resize-pixelwise t))
     (set-window-margins window 1 1)
     (set-frame-size frame width height t)
