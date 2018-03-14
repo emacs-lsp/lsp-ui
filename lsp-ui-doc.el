57a1ced86e200016e913682e8fba5579e4c508a4
Sebastien Chapuis <sebastien@chapu.is>
2018-03-14 12:13:17 +0100

    [lsp-ui-doc] Fix hook on load-theme #95

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -582,7 +582,7 @@ HEIGHT is the documentation number of lines."
   (unless (equal (ad-get-arg 0) (selected-window))
     (lsp-ui-doc--hide-frame)))
 
-(advice-add 'load-theme :before #'lsp-ui-doc--delete-frame)
+(advice-add 'load-theme :before (lambda (&rest _) (lsp-ui-doc--delete-frame)))
 (add-hook 'window-configuration-change-hook #'lsp-ui-doc--delete-frame)
 
 (defun lsp-ui-doc-enable-eldoc ()
