9b7d3c5ed3c9e0c4be07b0315ad2ce89e6703a8b
Erik Bredvold Bye <erikbye@protonmail.com>
2018-07-17 16:02:11 +0200

    set left-fringe width to 0 for ui-doc

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -579,7 +579,7 @@ HEIGHT is the documentation number of lines."
          (params (append lsp-ui-doc-frame-parameters
                          `((default-minibuffer-frame . ,(selected-frame))
                            (minibuffer . ,(minibuffer-window))
-                           (left-fringe . ,(frame-char-width))
+                           (left-fringe . 0)
                            (background-color . ,(face-background 'lsp-ui-doc-background nil t)))))
          (window (display-buffer-in-child-frame
                   buffer
