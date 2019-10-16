269871609a8db07ecd0889eaa83196201ada49e4
Ivan Yonchovski <yyoncho@users.noreply.github.com>
2019-10-16 19:44:55 +0300

    Revert "set left-fringe width to 0 for ui-doc (#157)"
    This reverts commit 4a0fd8034c2e57a4525c667648a91bf27f3e3403.

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -614,7 +614,7 @@ HEIGHT is the documentation number of lines."
                          `((name . "")
                            (default-minibuffer-frame . ,(selected-frame))
                            (minibuffer . ,(minibuffer-window))
-                           (left-fringe . 0)
+                           (left-fringe . ,(frame-char-width))
                            (background-color . ,(face-background 'lsp-ui-doc-background nil t)))))
          (window (display-buffer-in-child-frame
                   buffer
