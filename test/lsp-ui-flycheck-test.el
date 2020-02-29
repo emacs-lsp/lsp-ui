bf3966e87c91c5d825c483ce0883f30dd2272b62
brotzeit <brotzeitmacher@gmail.com>
2020-02-29 14:29:13 +0100

    lsp-ui-test-flycheck-list--update: remove unncecessary buffer

--- test/lsp-ui-flycheck-test.el
+++ test/lsp-ui-flycheck-test.el
@@ -5,18 +5,19 @@
   (let ((rustic-lsp-setup-p t)
         (rustic-lsp-server 'rust-analyzer))
     (setq lsp-log-io t)
-    (let* ((buffer (get-buffer-create "*lsp-diagnostics*"))
-           (string "fn main() {\nlet bar = 1;\nbar = bar + 2;}")
-           (dir (lsp-ui-test-create-project-buffer buffer string))
+    (let* ((string "fn main() {\nlet bar = 1;\nbar = bar + 2;}")
+           (buf (get-buffer-create "test-buffer"))
+           (dir (lsp-ui-test-create-project-buffer buf string))
            (file (concat dir "src/main.rs"))
-           (buf (get-file-buffer file))
            (diagnostics "2: rustc: value assigned to `bar` is never read
 2: rustc: cannot assign twice to immutable variable `bar`
 "))
       (sit-for 3)
-      (with-current-buffer buffer
+      (with-current-buffer buf
         (lsp-ui-flycheck-list--update (selected-window) (lsp-find-workspace 'rust-analyzer default-directory))
         (should (string= (buffer-substring-no-properties (point-min) (point-max)) diagnostics))
+        (print (buffer-name))
         (with-lsp-workspace (lsp-find-workspace 'rust-analyzer default-directory)
-          (lsp--shutdown-workspace))))))
+          (lsp--shutdown-workspace)))
+      (kill-buffer buf))))
 
