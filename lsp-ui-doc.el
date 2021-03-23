0cd6475f365265bb92613f6e0ccdd4426da171b8
Jen-Chieh Shen <jcs090218@gmail.com>
2021-03-23 23:01:09 +0800

    Fix by language specification (#591)
    * Fix by language specification
    
    * Remove debug msg

--- lsp-ui-doc.el
+++ lsp-ui-doc.el
@@ -302,7 +302,10 @@ Because some variables are buffer local.")
        (if (and language (not (string= "text" language)))
            (format "```%s\n%s\n```" language string)
          string))
-      (t (lsp--render-string (lsp-ui-doc--inline-formatted-string string) language))))))
+      ;; For other programming languages
+      (language (lsp--render-string (lsp-ui-doc--inline-formatted-string string) language))
+      ;; For default markdown content
+      (t (lsp--render-element (lsp-ui-doc--inline-formatted-string string)))))))
 
 (defun lsp-ui-doc--filter-marked-string (list-marked-string)
   "Filter the LIST-MARKED-STRING."
