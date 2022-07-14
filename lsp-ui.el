9a8983d95d823ae62e5f842a4bd433c860131398
brotzeit <brotzeitmacher@gmail.com>
2022-07-14 16:53:31 +0200

    bump version to 8.0.1

--- lsp-ui.el
+++ lsp-ui.el
@@ -7,7 +7,7 @@
 ;; Keywords: languages, tools
 ;; URL: https://github.com/emacs-lsp/lsp-ui
 ;; Package-Requires: ((emacs "26.1") (dash "2.18.0") (lsp-mode "6.0") (markdown-mode "2.3"))
-;; Version: 8.0.0
+;; Version: 8.0.1
 
 ;;; License
 ;;
