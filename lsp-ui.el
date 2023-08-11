0dd39900c8ed8145d207985cb2f65cedd1ffb410
Jen-Chieh Shen <jcs090218@gmail.com>
2023-08-10 22:52:55 -0700

    fix: Drop support for Emacs 26.x (#757)

--- lsp-ui.el
+++ lsp-ui.el
@@ -6,7 +6,7 @@
 ;; Author: Sebastien Chapuis <sebastien@chapu.is>, Fangrui Song <i@maskray.me>
 ;; Keywords: languages, tools
 ;; URL: https://github.com/emacs-lsp/lsp-ui
-;; Package-Requires: ((emacs "26.1") (dash "2.18.0") (lsp-mode "6.0") (markdown-mode "2.3"))
+;; Package-Requires: ((emacs "27.1") (dash "2.18.0") (lsp-mode "6.0") (markdown-mode "2.3"))
 ;; Version: 8.0.1
 
 ;;; License
