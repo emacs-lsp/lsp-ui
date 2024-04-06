8aa8b175fc4cdf2d16f6f3fdb2904e8874610c8a
JenChieh <jcs090218@gmail.com>
2024-04-06 14:19:55 -0700

    Bump version

--- lsp-ui.el
+++ lsp-ui.el
@@ -7,7 +7,7 @@
 ;; Keywords: languages, tools
 ;; URL: https://github.com/emacs-lsp/lsp-ui
 ;; Package-Requires: ((emacs "27.1") (dash "2.18.0") (lsp-mode "6.0") (markdown-mode "2.3"))
-;; Version: 8.0.1
+;; Version: 9.0.0
 
 ;;; License
 ;;
